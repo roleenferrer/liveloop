52f870ab99379ff10fd4836c147078ddbaa5c1af
David Ip <davidip100@yahoo.com>
2022-04-14 13:34:43 -0700

    backend stuff

---
+++ backend/src/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const yaml = require('js-yaml');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const path = require('path');
+const OpenApiValidator = require('express-openapi-validator');
+
+const bus = require('./bus');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const apiSpec = path.join(__dirname, '../api/openapi.yaml');
+
+const apidoc = yaml.load(fs.readFileSync(apiSpec, 'utf8'));
+app.use('/v0/api-docs', swaggerUi.serve, swaggerUi.setup(apidoc));
+
+app.use(
+    OpenApiValidator.middleware({
+      apiSpec: apiSpec,
+      validateRequests: true,
+      validateResponses: true,
+    }),
+);
+
+// Your routes go here - write them in a new component in it's own .js file
+// app.get('/v0/mail', email.getMailboxes);
+// app.get('/v0/mail/:id', email.getEmail);
+// app.post('/v0/mail', email.postEmail);
+// app.put('/v0/mail/:id', email.putEmail);
+
+app.use((err, req, res, next) => {
+  res.status(err.status).json({
+    message: err.message,
+    errors: err.errors,
+    status: err.status,
+  });
+});
+
+module.exports = app;
---
+++ backend/src/bus.js
@@ -0,0 +1,9 @@
+const {Pool} = require('pg');
+
+const pool = new Pool({
+  host: 'localhost',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+});
