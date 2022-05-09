6cf69deaafebc198dc038c1ab68fa322c2cc8961
David Ip <davidip100@yahoo.com>
2022-05-08 19:06:07 -0700

    Added directional flow to mock shuttles

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO Buses (bus_id, direction, longitude, latitude, active) VALUES (1, 'West', 5.0, 5.0, TRUE);
-INSERT INTO Buses (bus_id, direction, longitude, latitude, active) VALUES (2, 'West', 5.0, 5.0, TRUE);
+INSERT INTO Buses (bus_id, direction, longitude, latitude, active) VALUES (1, 'West', 0.0, 0.0, TRUE);
+INSERT INTO Buses (bus_id, direction, longitude, latitude, active) VALUES (2, 'West', 0.0, 0.0, TRUE);
+INSERT INTO Buses (bus_id, direction, longitude, latitude, active) VALUES (3, 'East', 0.0, 0.0, TRUE);
+INSERT INTO Buses (bus_id, direction, longitude, latitude, active) VALUES (4, 'East', 0.0, 0.0, TRUE);
--- backend/src/mockdata.js
+++ backend/src/mockdata.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3011, () => {
-  console.log('Mock data feed starting');
+  console.log('Mock data daemon starting');
 
   const {Pool} = require('pg');
   const pool = new Pool({
@@ -14,31 +14,58 @@ app.listen(3011, () => {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  const coordinates = [[1, 100], [2, 200], [3, 300], [4, 400], [5, 500],
+    [6, 400], [7, 300], [8, 200], [9, 100]];
+
+  const numShuttles = 4;
+
+  shuttlePos = [8, 6, 4, 1]; // The starting positions for each shuttle
+
+  const shuttleDirection = async (busID) => {
+    const getDirection = `SELECT direction
+                        FROM Buses
+                        WHERE bus_id = $1`;
+    const rootQuery = {
+      text: getDirection,
+      values: [busID],
+    };
+    const {rows} = await pool.query(rootQuery);
+    return rows[0].direction;
+  };
+
   /**
   * Simulate shuttle movement by updating bus coordinates periodically.
-   * https://stackoverflow.com/questions/45531690/how-to-create-an-infinite-loop-in-nodejs
-  * @param {int} counter
+  * https://stackoverflow.com/questions/45531690/how-to-create-an-infinite-loop-in-nodejs
   */
-  function updatePositions(counter) {
-    const coordinates = [[1, 100], [2, 200], [3, 300], [4, 400], [5, 500]];
-
-    for (let i = 1; i <= 2; i++) {
+  async function updatePositions() {
+    for (let i = 0; i < numShuttles; i++) {
       const select = `UPDATE buses
                       SET longitude = $1, latitude = $2
                       WHERE bus_id = $3`;
       const query = {
         text: select,
-        values: [coordinates[counter][0], coordinates[counter][1], i],
+        values: [coordinates[shuttlePos[i]][0],
+          coordinates[shuttlePos[i]][1], i + 1],
       };
       pool.query(query);
+
+      if (await shuttleDirection(i + 1) == 'West') {
+        if (shuttlePos[i] == coordinates.length - 1) {
+          shuttlePos[i] = 0;
+        } else {
+          shuttlePos[i] += 1;
+        }
+      } else if (await shuttleDirection(i + 1) == 'East') {
+        if (shuttlePos[i] == 0) {
+          shuttlePos[i] = coordinates.length - 1;
+        } else {
+          shuttlePos[i] -= 1;
+        }
+      }
     };
   };
 
-  counter = 0;
-
   setInterval(() => {
-    counter = counter % 5;
-    updatePositions(counter);
-    counter += 1;
-  }, 800);
+    updatePositions();
+  }, 3000);
 });
