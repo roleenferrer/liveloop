11c0988406d5f9b882c202526e1414593e4fe3e5
Roleen <roleenferrer@gmail.com>
2022-05-11 22:19:08 -0700

    added prototype

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,110 +1,104 @@
 import React from 'react'
-import GoogleMapReact from 'google-map-react'
+// import GoogleMap from 'google-map-react'
 import './style/map.css'
 
-import { Icon } from '@iconify/react'
-import locationIcon from '@iconify/icons-mdi/map-marker'
+// import { Icon } from '@iconify/react'
+// import locationIcon from '@iconify/icons-mdi/map-marker'
 
-const Map = ({ location, zoomLevel }) => (
-    <div className="map">
-      <div className="google-map">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyASTDssYDFH7WAYmpavSLgcBqFopJVf87w' }}
-          defaultCenter={location}
-          defaultZoom={zoomLevel}
-          options={{disableDefaultUI: true}}
-        >
-          <LocationPin
-            lat={location.lat}
-            lng={location.lng}
-            text={location.address}
-          />
-          <LocationPin
-            lat={36.9777223039389}
-            lng={-122.0536747707009}
-            text={"Main Entrance"}
-          />
-          <LocationPin
-              lat={36.981451629614874}
-              lng={-122.05195574654519}
-              text={"Lower Campus"}
-          />
-          <LocationPin
-              lat={36.98612000074854}
-              lng={-122.05347527298636}
-              text={"The Farm"}
-          />
-          <LocationPin
-              lat={36.99145429283725}
-              lng={-122.05464479513023}
-              text={"East Remote Parking"}
-          />
-          <LocationPin
-              lat={36.994304362434605}
-              lng={-122.05551753750245}
-              text={"East Field House"}
-          />
-          <LocationPin
-              lat={36.997539844191074}
-              lng={-122.05506544030388}
-              text={"Bookstore"}
-          />
-          <LocationPin
-              lat={36.99904725113633}
-              lng={-122.05514548284367}
-              text={"Crown/Merrill"}
-          />
-          <LocationPin
-              lat={36.99999651351248}
-              lng={-122.0583279747162}
-              text={"C9/10"}
-          />
-          <LocationPin
-              lat={37.00002893600196}
-              lng={-122.062329159318}
-              text={"Science Hill"}
-          />
-          <LocationPin
-              lat={36.99938734456224}
-              lng={-122.06454996723684}
-              text={"Kresge"}
-          />
-          <LocationPin
-              lat={36.99313410613227}
-              lng={-122.06514372032822}
-              text={"RCC/Kresge"}
-          />
-          <LocationPin
-              lat={36.99191322700488}
-              lng={-122.06679155373752}
-              text={"FSH"}
-          />
-          <LocationPin
-              lat={36.99004580052091}
-              lng={-122.06718599652353}
-              text={"Oakes"}
-          />
-          <LocationPin
-              lat={36.98289406053649}
-              lng={-122.06269942075527}
-              text={"Empire Grade"}
-          />
-          <LocationPin
-              lat={36.9788656482405}
-              lng={-122.05773923019366}
-              text={"High & Western"}
-          />
+import { InfoWindow, Marker, GoogleMap, LoadScript } from '@react-google-maps/api'
 
-        </GoogleMapReact>
-      </div>
-    </div>
-  )
+import data from "../components/data/test.json"
 
-const LocationPin = ({ text }) => (
-<div className="pin">
-    <Icon icon={locationIcon} className="pin-icon" />
-    <p className="pin-text">{text}</p>
-</div>
-)
+const center = {
+    lat: 36.988230225431984,
+    lng: -122.05822002436861
+  };
+
+const defaultPosition = {
+    lat: 36.9777223039389,
+    lng:-122.0536747707009
+};
+
+const onLoad = infoWindow => {
+    console.log('infoWindow: ', infoWindow)
+  }
+
+class Map extends React.Component {
+    state = {
+        markerposition: defaultPosition,
+        place: "Default"
+    }
+
+    onMouseOverEvent = (place) => {
+        this.setState({
+          markerposition: { lat: place.lat, lng: place.lng },
+          place: place.name,
+        });
+    };
+
+
+    render() {
+        return (
+            <div className="map">
+                <div className="google-map">
+                    <LoadScript
+                        googleMapsApiKey="AIzaSyASTDssYDFH7WAYmpavSLgcBqFopJVf87w">
+                        <GoogleMap
+                            mapContainerStyle={ {width: '400px', height: '400px'}}
+                            center={center}
+                            zoom={15}
+                        >
+                            <Marker opacity={0.2} position={ {lat:36.9777223039389, lng:-122.0536747707009} } />
+                            <Marker opacity={0.2} position={ {lat:36.981451629614874, lng:-122.05195574654519} } />
+                            <Marker opacity={0.2} position={ {lat:36.98612000074854, lng:-122.05347527298636} } />
+                            <Marker opacity={0.2} position={ {lat:36.99145429283725, lng:-122.05464479513023} } />
+                            <Marker opacity={0.2} position={ {lat:36.994304362434605, lng:-122.05551753750245} } />
+                            <Marker opacity={0.2} position={ {lat:36.997539844191074, lng:-122.05506544030388} } />
+                            <Marker opacity={0.2} position={ {lat:36.99904725113633, lng:-122.05514548284367} } />
+                            <Marker opacity={0.2} position={ {lat:36.99999651351248, lng:-122.0583279747162} } />
+                            <Marker opacity={0.2} position={ {lat:37.00002893600196, lng:-122.062329159318} } />
+                            <Marker opacity={0.2} position={ {lat:36.99938734456224, lng:-122.06454996723684} } />
+                            <Marker opacity={0.2} position={ {lat:36.99313410613227, lng:-122.06514372032822} } />
+                            <Marker opacity={0.2} position={ {lat:36.99191322700488, lng:-122.06679155373752} } />
+                            <Marker opacity={0.2} position={ {lat:36.99004580052091, lng:-122.06718599652353} } />
+                            <Marker opacity={0.2} position={ {lat:36.98289406053649, lng:-122.06269942075527} } />
+                            <Marker opacity={0.2} position={ {lat:36.9788656482405, lng:-122.05773923019366} } />
+
+                            <Marker 
+                             icon={{
+                                path:
+                                  "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+                                fillColor: "yellow",
+                                fillOpacity: 0.9,
+                                scale: 1,
+                                strokeColor: "gold",
+                                strokeWeight: 1,
+                              }}
+                            position = {this.state.markerposition}>  
+                            </Marker>
+                        
+                        
+                        </GoogleMap>
+                    </LoadScript>
+                    
+                    {data.map((place, index) => (
+                    <p key={index} onMouseOver={() => this.onMouseOverEvent(place)}>
+                        {place.name}
+                    </p>
+                    ))}
+                
+                </div>
+            </div>
+        )
+    }
+}
+
+
+// const LocationPin = ({ text }) => (
+// <div className="pin">
+//     <Icon icon={locationIcon} className="pin-icon" />
+//     <p className="pin-text">{text}</p>
+// </div>
+// )
 
 export default Map
