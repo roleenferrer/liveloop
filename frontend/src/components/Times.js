19899ba01d5e1f8f275998779e9192211aa70067
kpenners <kpenners@ucsc.edu>
2022-05-19 08:17:47 -0700

    Werking

--- frontend/src/components/Times.js
+++ frontend/src/components/Times.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React,{ useState } from "react";
 import Map from "./Map.jsx";
 import { makeStyles } from "@mui/styles";
-import Button from "@mui/material/Button";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 import { blueGrey } from "@mui/material/colors";
 import { Divider } from "@mui/material";
 import { Box } from "@mui/system";
+import data from "../components/data/test.json"
 
 const useStyles = makeStyles({
-  nav: {
+  dropone: {
     float: "left",
     width: "50%",
+    backgroundColor: 'lightgray'
   },
   map: {
     float: "right",
@@ -19,23 +20,56 @@ const useStyles = makeStyles({
   },
 });
 
-const useThemes = createTheme({
-  palette: {
-    feature: {
-      main: blueGrey[900],
-    },
-  },
-});
+//const [value, setValue] = React.useState('');
 
-function Tables() {
+//const handleChange = (event) => {
+ //   setValue(event.target.value);
+//};
 
-}
+const options = [
+  { label: 'Main Entrance', value: 'Main Entrance'},
+  { label: 'Lower Campus', value: 'Lower Campus'},
+  { label: 'The Farm', value: 'The Farm'},
+  { label: 'East Remote', value: 'East Remote'},
+  { label: 'East Field House', value: 'East Field House'},
+  { label: 'Bookstore', value: 'Bookstore'},
+  { label: 'Crown/Merril', value: 'Crown/Merril'},
+  { label: '9/10', value: '9/10'},
+  { label: 'Science Hill', value: 'Science Hill'},
+  { label: 'Kresge', value: 'Kresge'},
+  { label: 'RCC/Porter', value: 'RCC/Porter'},
+  { label: 'Oaks', value: 'Oaks'},
+  { label: 'Family Student Housing', value: 'Family Student Housing'},
+  { label: 'Empire Grade', value: 'Empire Grade'}
+];
+
+//const handleChange = (event) => {
+//    setValue(event.target.value);
+//};
 
 const Times = ({ location, zoom }) => {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <div> times?</div>
+      <div className={classes.dropone}>
+        <label>
+        Select starting location:   
+        <select>
+         {options.map((option) => (
+            <option value={option.value}>{option.label}</option>
+          ))}
+        </select>
+      </label>
+
+      <label>
+        Select destination:   
+        <select>
+          {options.map((option) => (
+            <option value={option.value}>{option.label}</option>
+          ))}
+        </select>
+      </label>
+      </div>
       <div className={classes.map}>
         <Map location={location} zoomLevel={zoom}></Map>
       </div>
