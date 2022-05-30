1d99a5dd42998c603db0fcf7717a0f6f19fe9747
David Ip <davidip100@yahoo.com>
2022-05-30 14:47:32 -0700

    Problem with scaling the icons

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -19,6 +19,12 @@ class Map extends React.Component {
         shuttle2: defaultPosition,
         shuttle3: defaultPosition,
         shuttle4: defaultPosition,
+        shuttle5: defaultPosition,
+        shuttle6: defaultPosition,
+        shuttle7: defaultPosition,
+        shuttle8: defaultPosition,
+        shuttle9: defaultPosition,
+        shuttle10: defaultPosition,
         place: "Default",
         digit: 0
     }
@@ -37,22 +43,34 @@ class Map extends React.Component {
                     return response.json();
                 })
                 .then((json) => {
-                    this.setState({shuttle1: {lat: json[0].longitude, lng: json[0].latitude}})
-                    this.setState({shuttle2: {lat: json[1].longitude, lng: json[1].latitude}})
-                    this.setState({shuttle3: {lat: json[2].longitude, lng: json[2].latitude}})
-                    this.setState({shuttle4: {lat: json[3].longitude, lng: json[3].latitude}})
+                    json.sort((a,b) => {
+                        if (a['bus_id'] < b['bus_id']) {
+                            return -1
+                        } else {
+                            return 1
+                        }
+                    });
+                    this.setState({shuttle1: {lat: json[0].latitude, lng: json[0].longitude}})
+                    this.setState({shuttle2: {lat: json[1].latitude, lng: json[1].longitude}})
+                    this.setState({shuttle3: {lat: json[2].latitude, lng: json[2].longitude}})
+                    this.setState({shuttle4: {lat: json[3].latitude, lng: json[3].longitude}})
+                    this.setState({shuttle5: {lat: json[4].latitude, lng: json[4].longitude}})
+                    this.setState({shuttle6: {lat: json[5].latitude, lng: json[5].longitude}})
+                    this.setState({shuttle7: {lat: json[6].latitude, lng: json[6].longitude}})
+                    this.setState({shuttle8: {lat: json[7].latitude, lng: json[7].longitude}})
+                    this.setState({shuttle9: {lat: json[8].latitude, lng: json[8].longitude}})
+                    this.setState({shuttle10: {lat: json[9].latitude, lng: json[9].longitude}})
                 })
                 .catch((error) => {
-                    alert(`ERROR: ${error.toString()}`);
+                    // alert(`ERROR: ${error.toString()}`);
                 });
-        }, 6000) // Increment every 2 Seconds
+        }, 700) // Increment every 6 Seconds
     }
 
     componentWillUnmount = () => {
         clearInterval(this.myTimer);
     }
     
-
     onMouseOverEvent = (place) => {
         this.setState({
           markerposition: { lat: place.lat, lng: place.lng },
@@ -60,14 +78,10 @@ class Map extends React.Component {
         });
     };
 
-
     render() {
         return (
             <div className="map">
                 <div className="google-map">
-                    {/* <div>
-                        Timer: {this.state.digit}
-                    </div> */}
                     <LoadScript
                         googleMapsApiKey="AIzaSyASTDssYDFH7WAYmpavSLgcBqFopJVf87w">
                         <GoogleMap
@@ -75,31 +89,17 @@ class Map extends React.Component {
                             center={center}
                             zoom={15}
                         >
-                            <Marker opacity={0.2} position={ {lat:36.9777223039389, lng:-122.0536747707009} } />
-                            <Marker opacity={0.2} position={ {lat:36.981451629614874, lng:-122.05195574654519} } />
-                            <Marker opacity={0.2} position={ {lat:36.98612000074854, lng:-122.05347527298636} } />
-                            <Marker opacity={0.2} position={ {lat:36.99145429283725, lng:-122.05464479513023} } />
-                            <Marker opacity={0.2} position={ {lat:36.994304362434605, lng:-122.05551753750245} } />
-                            <Marker opacity={0.2} position={ {lat:36.997539844191074, lng:-122.05506544030388} } />
-                            <Marker opacity={0.2} position={ {lat:36.99904725113633, lng:-122.05514548284367} } />
-                            <Marker opacity={0.2} position={ {lat:36.99999651351248, lng:-122.0583279747162} } />
-                            <Marker opacity={0.2} position={ {lat:37.00002893600196, lng:-122.062329159318} } />
-                            <Marker opacity={0.2} position={ {lat:36.99938734456224, lng:-122.06454996723684} } />
-                            <Marker opacity={0.2} position={ {lat:36.99313410613227, lng:-122.06514372032822} } />
-                            <Marker opacity={0.2} position={ {lat:36.99191322700488, lng:-122.06679155373752} } />
-                            <Marker opacity={0.2} position={ {lat:36.99004580052091, lng:-122.06718599652353} } />
-                            <Marker opacity={0.2} position={ {lat:36.98289406053649, lng:-122.06269942075527} } />
-                            <Marker opacity={0.2} position={ {lat:36.9788656482405, lng:-122.05773923019366} } />
-
-                            <Marker 
+                            <Marker
                              icon={{
                                 path:
-                                  "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+                                "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
                                 fillColor: "blue",
                                 fillOpacity: 0.9,
                                 scale: 1,
                                 strokeColor: "gold",
                                 strokeWeight: 1,
+                                // url: 'https://img.icons8.com/ios-glyphs/344/bus.png',
+                                // scaledSize: window.google.maps.Size(37, 37)
                               }}
                             position = {this.state.shuttle1}>  
                             </Marker>
@@ -121,7 +121,7 @@ class Map extends React.Component {
                              icon={{
                                 path:
                                   "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
-                                fillColor: "red",
+                                fillColor: "blue",
                                 fillOpacity: 0.9,
                                 scale: 1,
                                 strokeColor: "gold",
@@ -134,13 +134,91 @@ class Map extends React.Component {
                              icon={{
                                 path:
                                   "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+                                fillColor: "blue",
+                                fillOpacity: 0.9,
+                                scale: 1,
+                                strokeColor: "gold",
+                                strokeWeight: 1,
+                              }}
+                            position = {this.state.shuttle4}>
+                            </Marker>
+
+                            <Marker 
+                             icon={{
+                                path:
+                                  "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+                                fillColor: "blue",
+                                fillOpacity: 0.9,
+                                scale: 1,
+                                strokeColor: "gold",
+                                strokeWeight: 1,
+                              }}
+                            position = {this.state.shuttle5}>  
+                            </Marker>
+
+                            <Marker 
+                             icon={{
+                                path:
+                                  "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
                                 fillColor: "red",
                                 fillOpacity: 0.9,
                                 scale: 1,
                                 strokeColor: "gold",
                                 strokeWeight: 1,
                               }}
-                            position = {this.state.shuttle4}>  
+                            position = {this.state.shuttle6}>  
+                            </Marker>
+
+                            <Marker 
+                             icon={{
+                                path:
+                                  "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+                                fillColor: "red",
+                                fillOpacity: 0.9,
+                                scale: 1,
+                                strokeColor: "gold",
+                                strokeWeight: 1,
+                              }}
+                            position = {this.state.shuttle7}>  
+                            </Marker>
+
+                            <Marker 
+                             icon={{
+                                path:
+                                  "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+                                fillColor: "red",
+                                fillOpacity: 0.9,
+                                scale: 1,
+                                strokeColor: "gold",
+                                strokeWeight: 1,
+                              }}
+                            position = {this.state.shuttle8}>  
+                            </Marker>
+
+                            <Marker 
+                             icon={{
+                                path:
+                                  "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+                                fillColor: "red",
+                                fillOpacity: 0.9,
+                                scale: 1,
+                                strokeColor: "gold",
+                                strokeWeight: 1,
+                              }}
+                            position = {this.state.shuttle9}>  
+                            </Marker>
+
+                            <Marker 
+                             icon={{
+                                path:
+                                  "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+                                fillColor: "red",
+                                fillOpacity: 0.9,
+                                scale: 1,
+                                strokeColor: "gold",
+                                strokeWeight: 1,
+                              }}
+                            position = {this.state.shuttle10}>  
                             </Marker>
                        
                         </GoogleMap>
